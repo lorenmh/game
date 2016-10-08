29576bc76138bf6922163e84afcf76639652e8cf
loren <neutronstar@gmail.com>
2016-10-07 23:17:57 -0700

    test of grav

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[]) {
 	Body b1(0.0,0.0,1.0,1.0,1.0,0.0);
+	Body b2(0.0,0.0,1.0,1.0,1.0,0.0);
 	std::cout << b1.position.x << ", " << b1.position.y << '\n';
 	b1.move(1.0);
 	std::cout << b1.position.x << ", " << b1.position.y << '\n';
@@ -15,4 +16,8 @@ int main(int argc, char *argv[]) {
 	std::cout << b1.position.x << ", " << b1.position.y << '\n';
 	b1.move(1.0);
 	std::cout << b1.position.x << ", " << b1.position.y << '\n';
+	Vec2 g1 = b1.gravity(b2);
+	Vec2 g2 = b2.gravity(b1);
+	std::cout << "g1: " << g1.x << ", " << g1.y << '\n';
+	std::cout << "g2: " << g2.x << ", " << g2.y << '\n';
 }
